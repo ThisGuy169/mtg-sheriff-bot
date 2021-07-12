9a2048b629bf01c195d289e84c9461ad5427d009
ThisGuy169 <aderrick318@gmail.com>
2021-07-11 20:27:13 -0500

    Added client to command calls

--- src/bot.js
+++ src/bot.js
@@ -22,7 +22,7 @@ client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`../commands/${file}`);
 	// set a new item in the Collection
 	// with the key as the command name and the value as the exported module
 	client.commands.set(command.name, command);
@@ -40,11 +40,11 @@ client.on('message', message => {
     const command = client.commands.get(commandName);
 
     try {
-        command.execute(message, args);
+        command.execute(client, message, args);
     }
     catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+      console.error(error);
+      message.reply('there was an error trying to execute that command!');
     }
 
 });
