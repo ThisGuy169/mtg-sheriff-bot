841ea12d44909c868294a0de7eba4631ad1e31d4
Supermanawl <alott1925@gmail.com>
2021-07-16 18:20:03 -0500

    Add Player to Game class

--- src/game.js
+++ src/game.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const Player = require('../src/player.js');
 const Role = require('../src/role.js');
 module.exports = class Game{
     constructor(creator, createdOn, players){
@@ -32,4 +33,38 @@ module.exports = class Game{
         return roleList;
     }
 
+    assignPlayers(mentions, roles){
+		let list = [];
+		let person = null;
+        let role = null;
+		let index = null;
+
+		players.forEach(function(player){
+            person = player;
+            role = roles[Math.floor(Math.random() * roles.length)];
+            list.push(new Player(person, role));
+			index = roles.indexOf(role);
+			if (index !== -1) {
+				roles.splice(index, 1);
+			}
+		});
+		// return list of assign Players
+		return list;
+	}
+
+    messagePlayers(){
+		players.forEach(player => {
+			let message = new MessageEmbed();
+			message.setColor(Role.colors[player.Role.Color]);
+			message.setDescription(player.Role.Description);
+			message.setTitle(player.Role.Title);
+			// message.setThumbnail(player.Role.Image);
+			// message.setImage(player.Role.Image);
+			// p.person.send(embed=embedVar, file=file)
+			console.log(message)
+			//player.Person.send(message)
+
+		})
+		
+	}
 }
--- src/player.js
+++ src/player.js
@@ -6,41 +6,4 @@ module.exports = class Player{
 		this.Person = person;
 		this.Role = role;
 	}
-
-	assignPlayers(mentions, roles){
-		let list = [];
-		let person = null;
-        let role = null;
-		let index = null;
-
-		mentions.forEach(function(mention){
-            person = mention;
-            role = roles[Math.floor(Math.random() * roles.length)];
-            list.push(new Player(person, role));
-			index = roles.indexOf(role);
-			if (index !== -1) {
-				roles.splice(index, 1);
-			}
-		});
-		// return list of assign Players
-		return list;
-	}
-
-	messagePlayers(playerList)
-	{
-		playerList.forEach(player => {
-			let message = new MessageEmbed();
-			message.setColor(Role.colors[player.Role.Color]);
-			message.setDescription(player.Role.Description);
-			message.setTitle(player.Role.Title);
-			// message.setThumbnail(player.Role.Image);
-			// message.setImage(player.Role.Image);
-			// p.person.send(embed=embedVar, file=file)
-			console.log(message)
-			//player.Person.send(message)
-
-		})
-		
-	}
-
 }
