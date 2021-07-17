3180ff434b7c2770e5d7a87a084325be944cdfd6
Supermanawl <alott1925@gmail.com>
2021-07-16 20:17:25 -0500

    Remove import from role class

--- src/role.js
+++ src/role.js
@@ -1,8 +1,5 @@
-const fs = require('fs');
-
 module.exports =  class Role{
     static colors = {"gold": 0xf1c40f, "lightGreen": 0x00ff00, "blue": 0x3498db, "red": 0xe74c3c, "darkGray":0x607d8b }
-
     constructor(roleID, title, description, image, color) {
 		this.RoleID = roleID;
 		this.Title = title;
